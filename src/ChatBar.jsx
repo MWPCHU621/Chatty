bd5d6542ae3aeb97d37082af0b76525a79e9c4e2
Patrick Chu <mingweipatrickc@gmail.com>
2018-10-02 14:11:39 +0000

    set up initial state of chatty app

---
+++ src/ChatBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class ChatBar extends React.Component {
+  render() {
+    return(
+      <footer className="chatbar">
+        <input className="chatbar-username" placeholder="Your Name (Optional)" />
+        <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+      </footer>
+    );
+  }
+}
+
+export default ChatBar;
---
+++ src/MessageList.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import Message from './Message.jsx';
+
+class MessageList extends Component {
+  render() {
+    return(
+      <Message/>
+    );
+  }
+}
+
+export default MessageList;
